72eb0b7014a557c419810ffa12a1b9c03ad537f2
iuliaotilia26 <mustea.iulia@gmail.com>
2019-07-14 17:17:50 +0200

    Added alpha_c weight calculation in the bokeh effect

--- app/src/main/cpp/BokehEffect.h
+++ app/src/main/cpp/BokehEffect.h
@@ -23,6 +23,10 @@ class BokehEffect{
 		double _focalLength=0.2; // f in the paper
 		double _aperture=0.01; // A in the paper
 
+	    double _wfullframe = 3.6; //36 mm
+
+	    double _wsensor = 4; //defined by the user, just tried 4
+
 		double _dFocus = 1;
 
 		unsigned int _cocListSize = 3;
@@ -39,6 +43,8 @@ public:
 		double & focalLenth(){return _focalLength;}
 		double & aperture(){return _aperture;}
 		double & dFocus(){ return _dFocus;}
+		double & wfullframe(){ return _wfullframe;}
+		double & wsensor() { return _wsensor;}
 
 		unsigned int cocListSize() {return _cocListSize;}
 
@@ -47,6 +53,14 @@ public:
 			return std::abs(_aperture*_focalLength*(_dFocus-depth)/(depth*(_dFocus -_focalLength)));
 		}
 
+		double thetahat(const cv::Vec2i p)
+		{
+			const auto depth = _depthInput.at<double>(p);
+			const auto upper_term = (_rgbInput.cols * _wsensor * _focalLength * _focalLength* _wfullframe * _wfullframe * (_dFocus - depth)) / (_wsensor *  _wsensor);
+			const auto lower_term = _wfullframe * (_focalLength / _aperture) * depth * (_dFocus * _wfullframe - _focalLength * _wfullframe);
+			return std::abs(upper_term / lower_term);
+
+		}
 
 
 		void compute();
